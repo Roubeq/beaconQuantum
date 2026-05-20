e8964a2884511b1178d67b67042c3a661b918f43
Roubeq <arteem.blinov.2005@gmail.com>
2026-05-20 17:08:00 +0300

    #1 add colors/theme

--- app/src/main/java/com/example/beaconquantum/ui/theme/Color.kt
+++ app/src/main/java/com/example/beaconquantum/ui/theme/Color.kt
@@ -2,10 +2,57 @@ package com.example.beaconquantum.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryContainer = Color(0xFFEADDFF)
+val OnPrimaryContainer = Color(0xFF4F378A)
+val PrimaryFixed = Color(0xFFEADDFF)
+val PrimaryFixedVariant = Color(0xFF4F378A)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Secondary = Color(0xFF625B71)
+val SecondaryContainer = Color(0xFFE8DEF8)
+val OnSecondaryContainer = Color(0xFF4A4459)
+val SecondaryFixed = Color(0xFFE8DEF8)
+val SecondaryFixedContainer = Color(0xFF4A4459)
+
+val OnSurfaceVariant = Color(0xFF49454F)
+
+val Background = Color(0xFFFFFFFF)
+val OnBackground = Color(0xFF1D1B20)
+val OnSurface = Color(0xFF1D1B20)
+
+val SurfaceContainerLow = Color(0xFFFFFFFF)
+val SurfaceContainer = Color(0xFFF3EDF7)
+val SurfaceContainerHigh = Color(0xFFECE6F0)
+
+val DarkTextIcon = Color(0xFF1E1E1E)     // Для Text/Default/Default и кучи иконок
+val GrayDisabled = Color(0xFFD9D9D9)     // Для заблокированных элементов / Border
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+
+// Акценты (Семантические цвета для статусов, графиков или категорий)
+val AccentGreen = Color(0xFF16A11D)
+val AccentOrange = Color(0xFFE89A09)
+val AccentBlue = Color(0xFF20AAFF)
+val AccentTeal = Color(0xFF12AB9F)
+val AccentRed = Color(0xFFFF2B2B)
+
+// Цвета для темной темы
+val PrimaryDark = Color(0xFFD0BCFF)
+val OnPrimaryDark = Color(0xFF381E72)
+val PrimaryContainerDark = Color(0xFF4F378A)
+val OnPrimaryContainerDark = Color(0xFFEADDFF)
+
+val SecondaryDark = Color(0xFFCCC2DC)
+val OnSecondaryDark = Color(0xFF332D41)
+val SecondaryContainerDark = Color(0xFF4A4459)
+val OnSecondaryContainerDark = Color(0xFFE8DEF8)
+
+val BackgroundDark = Color(0xFF1C1B1F)
+val OnBackgroundDark = Color(0xFFE6E1E5)
+val SurfaceDark = Color(0xFF1C1B1F)
+val OnSurfaceDark = Color(0xFFE6E1E5)
+val SurfaceVariantDark = Color(0xFF49454F)
+val OnSurfaceVariantDark = Color(0xFFCAC4D0)
+
+val SurfaceContainerLowDark = Color(0xFF1D1B20)
+val SurfaceContainerDark = Color(0xFF211F26)
+val SurfaceContainerHighDark = Color(0xFF2B2930)
